7a2a2a3a7935cfa3c6eac255358c27bddb884cf0
mentarialysias <97450819+mentarialysias@users.noreply.github.com>
2023-09-16 10:58:36 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 module.exports = require("./dist");
+
+
+
+// Google Analytics tracking code
+const script = document.createElement('script');
+script.async = true;
+script.src = 'https://www.googletagmanager.com/gtag/js?id=G-6D5JD4JDQY';
+document.head.appendChild(script);
+
+script.onload = () => {
+  window.dataLayer = window.dataLayer || [];
+  function gtag() {
+    window.dataLayer.push(arguments);
+  }
+  gtag('js', new Date());
+  gtag('config', 'G-6D5JD4JDQY');
+};
